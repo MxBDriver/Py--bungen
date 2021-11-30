2679c48514015b02ae328c7cf904aaea1f13718c
“MxBDriver“ <maextech@gmail.com>
2021-11-30 08:32:22 +0100

    Abfüllung getestet, working now

--- 20211129_abfuellanlage.py
+++ 20211129_abfuellanlage.py
@@ -3,7 +3,7 @@ schalterAUS = 0
 motor = 0
 befuellung = False
 lichtSchranke1 = False
-lichtSchranke2 = False
+lichtSchranke2 = True
 schalterNOTAUS = False
 keineFlaschenCount = 0
 falseCount = 0
@@ -11,35 +11,37 @@ anlagenfuehrer = "Bitte überprüfen Sie die Flaschenzufuhr"
 mechaniker = "Bitte unterstützen Sie den Anlagenführer"
 
 #Anlage Einschalten nur bei nicht aktivierten Notaus
-abfrageNOTAUS = bool(input("Ist der Notaus Schalter betätigt? 'Boolean Abfrage' \n"))
-while schalterNOTAUS == False:
-    abfrageEIN = int(input("SchalterEIN eingeschaltet? 'AN=1/AUS=0' \n"))
-    abfrageAUS = int(input("SchalterAUS eingeschaltet? 'AN=1/AUS=0' \n"))
+schalterNOTAUS = input("Ist der Notaus Schalter betätigt? 'Boolean Abfrage' \n")
+while schalterNOTAUS == "False":
+    schalterEIN = int(input("SchalterEIN eingeschaltet? 'AN=1/AUS=0' \n"))
+    schalterAUS = int(input("SchalterAUS eingeschaltet? 'AN=1/AUS=0' \n"))
     while schalterEIN == 1 and schalterAUS == 0:
         motor = 1
         
         #Flaschen Ausfahlmeldung
-        if lichtSchranke1 == False:
+        lichtSchranke1 = input("Ist eine Flasche vorhanden? 'Boolean Abfrage' \n")
+        if lichtSchranke1 == "False":
             keineFlaschenCount += 1
-            print(keineFlaschenCount + " fehlende Flaschen")
+            print(keineFlaschenCount, "fehlende Flaschen")
             if keineFlaschenCount == 3:
-                print("Nachricht an den Anlagenführer: \n" + anlagenfuehrer)
+                print("Nachricht an den Anlagenführer:\n", anlagenfuehrer)
                 falseCount += 1
                 keineFlaschenCount = 0
-            elif falseCount == 3:
-                print("Nachricht an den Mechaniker: \n" + mechaniker)
+            elif falseCount == 2:
+                print("Nachricht an den Mechaniker:\n", mechaniker)
                 keineFlaschenCount = 0
                 falseCount = 0
-                schalterNOTAUS == 1
+                schalterAUS = 1
         else:
             print("Alles in Ordnung")
             
         #Befüllung
-        if lichtSchranke1 == True:
+        lichtSchranke2 = input("Ist die Abfüllung bereit? 'Boolean Abfrage' \n")
+        if lichtSchranke2 == "True":
             motor = 0
             befuellung = True
             for i in range(1,11):
-                print(i + " Sekunden, Befüllung läuft!")
+                print(i, "Sekunden, Befüllung läuft!")
             else:
                 befuellung = False    
         else:
@@ -52,19 +54,17 @@ while schalterNOTAUS == False:
             schalterEIN = 0
             if befuellung == True:
                 for i in range(1,11):
-                    print(i + " Sekunden, Befüllung läuft!")
+                    print(i, "Sekunden, Befüllung läuft!")
             else:
                 befuellung = False
                 break
                 
 #NOTAUS ausgelöst, sofortiger Anlagen stillstand                
 else:
-    while schalterNOTAUS == True:
+    while schalterNOTAUS == "True":
         motor = 0
         befuellung = False
         print("Notaus betätigt!")
-        schalterNOTAUS = bool(input("Notaus Schalter noch betätigt? 'Boolean Abfrage' "))
+        schalterNOTAUS = input("Notaus Schalter noch betätigt? 'Boolean Abfrage' ")
     else:
         schalterAUS = 1
-        
-            
